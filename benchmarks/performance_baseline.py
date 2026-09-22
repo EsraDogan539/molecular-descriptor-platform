e98cdc0edafbb23d8d11e81ba96f7e9e99b89cb0
esra doğan <esra.6615@gmail.com>
2026-09-22 10:15:10 +0300

    Fix benchmark module path in CI

--- benchmarks/performance_baseline.py
+++ benchmarks/performance_baseline.py
@@ -1,5 +1,11 @@
 import argparse
+import os
+import sys
 import time
+
+ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
 
 import pandas as pd
 
